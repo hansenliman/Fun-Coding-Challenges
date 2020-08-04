dc28da565eee1586958d45e612fa3e1d2b7fe2de
Hansen Liman <hansenliman@gmail.com>
2020-08-04 05:01:19 -0700

    Update Different Ways to Add Parentheses.py

--- Different Ways to Add Parentheses.py
+++ Different Ways to Add Parentheses.py
@@ -3,7 +3,8 @@ Problem taken from LeetCode
 
 Different Ways to Add Parentheses
 
-Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
+Given a string of numbers and operators, return all possible results from computing all the 
+different possible ways to group numbers and operators. The valid operators are +, - and *.
 
 Example 1:
 
